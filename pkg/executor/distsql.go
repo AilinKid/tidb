c906375710072c0dd5f6079fe28595398e7c9003
Yiding Cui <winoros@gmail.com>
2025-11-25 01:10:05 +0800

    executor: tici range may be wrong, casued by resolving merge conflict (#64652)
    ref pingcap/tidb#61185

--- pkg/executor/distsql.go
+++ pkg/executor/distsql.go
@@ -1143,11 +1143,6 @@ func (e *IndexLookUpExecutor) buildIndexSelectResultForRange(
 		builder.FullTextInfo.TableID = e.table.Meta().ID
 		builder.FullTextInfo.IndexID = e.index.ID
 		builder.FullTextInfo.ExecutorID = e.idxPlans[0].ExplainID().String()
-
-		id := e.Table().Meta().ID
-		startKey := tablecodec.EncodeTablePrefix(id)
-		endKey := tablecodec.EncodeTablePrefix(id + 1)
-		kvRange = []kv.KeyRange{{StartKey: startKey, EndKey: endKey}}
 	}
 
 	if e.indexLookUpPushDown {
