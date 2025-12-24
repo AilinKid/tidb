dce0092776cb6979400683b0c460886146708d22
OliverS929 <182192954+OliverS929@users.noreply.github.com>
2025-12-25 02:49:37 +0800

    ddl: support sharding key in Hybrid index Parameter (#65201)

--- pkg/meta/model/job_args.go
+++ pkg/meta/model/job_args.go
@@ -1402,7 +1402,7 @@ func (a *ModifyIndexArgs) getArgsV1(job *Job) []any {
 	// Add Full text index or hybrid index
 	if job.Type == ActionAddFullTextIndex || job.Type == ActionAddHybridIndex {
 		arg := a.IndexArgs[0]
-		return []any{arg.IndexName, arg.IndexPartSpecifications[0], arg.IndexOption}
+		return []any{arg.IndexName, arg.IndexPartSpecifications, arg.IndexOption}
 	}
 
 	// Add primary key
@@ -1563,19 +1563,24 @@ func (a *ModifyIndexArgs) decodeAddVectorIndexV1(job *Job) error {
 
 func (a *ModifyIndexArgs) decodeAddFullTextIndexV1(job *Job) error {
 	var (
-		indexName              pmodel.CIStr
-		indexPartSpecification *ast.IndexPartSpecification
-		indexOption            *ast.IndexOption
+		indexName               pmodel.CIStr
+		indexPartSpecification  *ast.IndexPartSpecification
+		indexPartSpecifications []*ast.IndexPartSpecification
+		indexOption             *ast.IndexOption
 	)
 
 	if err := job.decodeArgs(
-		&indexName, &indexPartSpecification, &indexOption); err != nil {
-		return errors.Trace(err)
+		&indexName, &indexPartSpecifications, &indexOption); err != nil {
+		if err = job.decodeArgs(
+			&indexName, &indexPartSpecification, &indexOption); err != nil {
+			return errors.Trace(err)
+		}
+		indexPartSpecifications = []*ast.IndexPartSpecification{indexPartSpecification}
 	}
 
 	a.IndexArgs = []*IndexArg{{
 		IndexName:               indexName,
-		IndexPartSpecifications: []*ast.IndexPartSpecification{indexPartSpecification},
+		IndexPartSpecifications: indexPartSpecifications,
 		IndexOption:             indexOption,
 	}}
 	return nil
@@ -1583,19 +1588,24 @@ func (a *ModifyIndexArgs) decodeAddFullTextIndexV1(job *Job) error {
 
 func (a *ModifyIndexArgs) decodeAddHybridIndexV1(job *Job) error {
 	var (
-		indexName              pmodel.CIStr
-		indexPartSpecification *ast.IndexPartSpecification
-		indexOption            *ast.IndexOption
+		indexName               pmodel.CIStr
+		indexPartSpecification  *ast.IndexPartSpecification
+		indexPartSpecifications []*ast.IndexPartSpecification
+		indexOption             *ast.IndexOption
 	)
 
 	if err := job.decodeArgs(
-		&indexName, &indexPartSpecification, &indexOption); err != nil {
-		return errors.Trace(err)
+		&indexName, &indexPartSpecifications, &indexOption); err != nil {
+		if err = job.decodeArgs(
+			&indexName, &indexPartSpecification, &indexOption); err != nil {
+			return errors.Trace(err)
+		}
+		indexPartSpecifications = []*ast.IndexPartSpecification{indexPartSpecification}
 	}
 
 	a.IndexArgs = []*IndexArg{{
 		IndexName:               indexName,
-		IndexPartSpecifications: []*ast.IndexPartSpecification{indexPartSpecification},
+		IndexPartSpecifications: indexPartSpecifications,
 		IndexOption:             indexOption,
 	}}
 	return nil
