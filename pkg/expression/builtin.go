2c40bb14c69f5ab57172ec2798a5d3c7fa776dd0
Yiding Cui <winoros@gmail.com>
2026-02-03 18:11:52 +0800

    *: add FTS_MATCH_PHRASE function (#65831)
    ref pingcap/tidb#64651

--- pkg/expression/builtin.go
+++ pkg/expression/builtin.go
@@ -970,6 +970,7 @@ var funcs = map[string]functionClass{
 	// FTS functions.
 	ast.FTSMatchWord:   &ftsMatchWordFunctionClass{baseFunctionClass{ast.FTSMatchWord, 2, 2}},
 	ast.FTSMatchPrefix: &ftsMatchPrefixFunctionClass{baseFunctionClass{ast.FTSMatchPrefix, 2, -1}},
+	ast.FTSMatchPhrase: &ftsMatchPhraseFunctionClass{baseFunctionClass{ast.FTSMatchPhrase, 2, -1}},
 
 	// TiDB internal function.
 	ast.TiDBDecodeKey:       &tidbDecodeKeyFunctionClass{baseFunctionClass{ast.TiDBDecodeKey, 1, 1}},
--- pkg/parser/ast/functions.go
+++ pkg/parser/ast/functions.go
@@ -369,6 +369,7 @@ const (
 	// FTS functions (TiDB extension).
 	FTSMatchWord   = "fts_match_word"
 	FTSMatchPrefix = "fts_match_prefix"
+	FTSMatchPhrase = "fts_match_phrase"
 
 	// TiDB internal function.
 	TiDBDecodeKey       = "tidb_decode_key"
